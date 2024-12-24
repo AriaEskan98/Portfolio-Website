35c8b163415cd101ac0e85d38358b62e9cf4c169
Aria Eskandarzadeh <esk0003@vsb.cz>
2024-12-24 10:58:51 +0000

    Added the Speed Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
         <Footer/>
         <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div>
         <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
+        <SpeedInsights />
       </body>
       <PrismicPreview repositoryName={repositoryName}/>
     </html>
