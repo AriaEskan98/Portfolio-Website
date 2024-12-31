8cc5ac0edc3e4967fa80f67fd08ad376cba91f7b
Aria Eskandarzadeh <esk0003@vsb.cz>
2024-12-31 19:09:39 +0000

    fixed div again

--- src/slices/ButtonLink/index.tsx
+++ src/slices/ButtonLink/index.tsx
@@ -17,7 +17,7 @@ const ButtonLink = ({ slice }: ButtonLinkProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="grid gap-x-8 gap-y-6 md:grid-cols[2fr,1fr]">
+      <div className="flex flex-wrap gap-4">
       <Button linkField={slice.primary.project_link} label={slice.primary.button_text}></Button>
       </div>
 
