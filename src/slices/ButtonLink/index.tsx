54ef0ae46b7be531edeacaf31e1f88aaf0a539bb
Aria Eskandarzadeh <esk0003@vsb.cz>
2024-12-31 19:17:43 +0000

    div for buttonLink

--- src/slices/ButtonLink/index.tsx
+++ src/slices/ButtonLink/index.tsx
@@ -1,4 +1,3 @@
-import Bounded from "@/components/Bounded";
 import Button from "@/components/Button";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
@@ -13,15 +12,12 @@ export type ButtonLinkProps = SliceComponentProps<Content.ButtonLinkSlice>;
  */
 const ButtonLink = ({ slice }: ButtonLinkProps): JSX.Element => {
   return (
-    <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <div className="flex flex-wrap gap-4">
-      <Button linkField={slice.primary.project_link} label={slice.primary.button_text}></Button>
-      </div>
-
-    </Bounded>
+    <div className="inline-flex gap-4">
+      <Button
+        linkField={slice.primary.project_link}
+        label={slice.primary.button_text}
+      />
+    </div>
   );
 };
 
